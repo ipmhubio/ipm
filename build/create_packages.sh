a3bc9af4bd01f56a9e3ab93edeeff44120e23190
Bas <61052187+BasBerk@users.noreply.github.com>
2024-03-24 21:09:06 +0100

    New IPM version 0.5.4

--- build/create_packages.sh
+++ build/create_packages.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Check if the script received two arguments
-if [ "$#" -ne 1 ]; then
-    echo "You must enter exactly 1 command line arguments"
+# Check if the script received at least one argument
+if [ "$#" -lt 1 ]; then
+    echo "You must enter at least 1 command line argument"
     exit 1
 fi
-# Assign the second argument to a variable named input_path
-input_path=$1
 
 # Create the export directory if it doesn't exist
 mkdir -p export
@@ -14,21 +12,27 @@ mkdir -p export
 # Get the absolute path of the export directory
 export_path=$(pwd)/export
 
-# Start a loop that iterates over each directory in the input_path
-for dir in "$input_path"/*/ ; do
-    # Remove trailing slash from directory name
-    dir=${dir%/}
+# Clear the packages.md file
+echo "" > packages.md
 
-    # Get the folder name from the directory path
-    foldername=$(basename "$dir")
+# Start a loop that iterates over each argument
+for input_path in "$@" ; do
+    # Start a nested loop that iterates over each directory in the input_path
+    for dir in "$input_path"/*/ ; do
+        # Remove trailing slash from directory name
+        dir=${dir%/}
 
-    # Create a zip file with the name ipm-${foldername}.zip in the export directory
-    # Run the zip command from the parent directory of the directory that's being zipped
-    (cd "$dir/.." && zip -r "$export_path/ipm-${foldername}.zip" "$foldername")
+        # Get the folder name from the directory path
+        foldername=$(basename "$dir")
 
-    # Calculate its sha256 hash
-    hash=$(shasum -a 256 "$export_path/ipm-${foldername}.zip" | awk '{ print $1 }')
-    
-    # Write the filename and hash to the markdown file
-    echo -e "* ipm-${foldername}.zip\n\t * $hash" >> packages.md
+        # Create a zip file with the name ipm-${foldername}.zip in the export directory
+        # Run the zip command from the directory that's being zipped and specify the ipm or ipm.exe file
+        (cd "$dir" && zip -r "$export_path/ipm-${foldername}.zip" ipm* )
+
+        # Calculate its sha256 hash
+        hash=$(shasum -a 256 "$export_path/ipm-${foldername}.zip" | awk '{ print $1 }')
+        
+        # Write the filename and hash to the markdown file
+        echo -e "* ipm-${foldername}.zip\n\t * $hash" >> packages.md
+    done
 done
