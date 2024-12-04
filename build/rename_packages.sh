8435b5335d8fd394c6bc02dc32905d6ee9ac6054
Bas <61052187+BasBerk@users.noreply.github.com>
2024-12-04 22:43:50 +0100

    fix: rename releasenote.md to releasenotes.md and update content generation

--- build/rename_packages.sh
+++ build/rename_packages.sh
@@ -25,14 +25,14 @@ for input_folder in "$@" ; do
                 mkdir -p "ipm-releases/$version"
                 # Copy and rename the file to the ipm-releases/{version} directory
                 cp "$input_file" "ipm-releases/$version/$new_name"
-                # Create the releasenote.md file with the specified content
-                echo "## $version ($(date +%m/%d/%Y))" > "ipm-releases/$version/releasenote.md"
-                echo "" >> "ipm-releases/$version/releasenote.md"
-                echo "#### IPM $version Release Notes" >> "ipm-releases/$version/releasenote.md"
-                echo "**New features**:" >> "ipm-releases/$version/releasenote.md"
-                echo "**Enhancements**:" >> "ipm-releases/$version/releasenote.md"
-                echo "**Bug fixes**:" >> "ipm-releases/$version/releasenote.md"
-                echo "----" >> "ipm-releases/$version/releasenote.md"
+                # Create the releasenotes.md file with the specified content
+                echo "## $version ($(date +%m/%d/%Y))" > "ipm-releases/$version/releasenotes.md"
+                echo "" >> "ipm-releases/$version/releasenotes.md"
+                echo "#### IPM $version Release Notes" >> "ipm-releases/$version/releasenotes.md"
+                echo "**New features**:" >> "ipm-releases/$version/releasenotes.md"
+                echo "**Enhancements**:" >> "ipm-releases/$version/releasenotes.md"
+                echo "**Bug fixes**:" >> "ipm-releases/$version/releasenotes.md"
+                echo "----" >> "ipm-releases/$version/releasenotes.md"
             else
                 # If no version number is found, keep the original name
                 new_name="$base_name"
@@ -50,8 +50,8 @@ done
 for file in "ipm-releases/$version"/* ; do
     if [[ "$(basename "$file")" != *.sha256 && "$(basename "$file")" != *.md ]]; then
         file_hash=$(sha256sum "$file" | awk '{ print $1 }')
-        echo "* $(basename "$file")" >> "ipm-releases/$version/releasenote.md"
-        echo "    * $file_hash" >> "ipm-releases/$version/releasenote.md"
+        echo "* $(basename "$file")" >> "ipm-releases/$version/releasenotes.md"
+        echo "    * $file_hash" >> "ipm-releases/$version/releasenotes.md"
     fi
 done
 echo "Files have been copied and renamed successfully."
